8caa17904a75fa42361d0ff43f0690c65cd2452b
__AltafShaikh <128633184+altaf-ts@users.noreply.github.com>
2024-10-12 16:53:57 +0530

    second commit

--- src/components/IndustryLogos.jsx
+++ src/components/IndustryLogos.jsx
@@ -5,9 +5,9 @@ import './IndustryLogos.css';
 const IndustryLogos = () => {
   return (
     <div className="logos-container">
-      <h3></h3>
+      
       <div className="logos">
-        <img src="Text.png" alt="Industry Logos" className="all-logos" />
+        <img src="text.png" alt="Industry Logos" className="all-logos" />
       </div>
     </div>
   );
